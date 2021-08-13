a38aa1985e727959238a5d16869e632715e8c218
debaniya-n <pururualma@gmail.com>
2021-08-13 14:26:14 +0700

    Initial commit

---
+++ Latihan1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan1 : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("ayayayayayayaya");
+    }
+
+    void Update()
+    {
+        
+    }
+}
